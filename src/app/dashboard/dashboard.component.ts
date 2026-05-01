922b8e210b5f25000892aeb0f394b46442c41f2b
JoseReyesY <92343754+JoseReyesY@users.noreply.github.com>
2026-05-01 16:34:36 -0600

    active projects number in dashboard implemented

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// Service
+import { ProjectsService } from '../projects/projects.service';
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -10,6 +13,11 @@ import { Component } from '@angular/core';
 export class DashboardComponent {
   totalHours = 0;
   totalHoursPerWeek = 0;
-  activeProjects = 0;
   streakDays = 0;
+
+  constructor(private projectsService: ProjectsService) {}
+
+  get activeProjects() {
+    return this.projectsService.getProjects().length;
+  }
 }
